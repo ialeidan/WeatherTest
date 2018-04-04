80c8ac599fe877c993c9ef8a9de6ad8da302dfe7
AsimNet <hlh132132@gmail.com>
2018-04-05 00:37:03 +0300

    cleaning Moya class

--- WeatherTest/Cities/Services/WeatherService.swift
+++ WeatherTest/Cities/Services/WeatherService.swift
@@ -3,19 +3,7 @@
 import Foundation
 import Moya
 
-// MARK: - Provider setup
-
-private func JSONResponseDataFormatter(_ data: Data) -> Data {
-    do {
-        let dataAsJSON = try JSONSerialization.jsonObject(with: data)
-        let prettyData =  try JSONSerialization.data(withJSONObject: dataAsJSON, options: .prettyPrinted)
-        return prettyData
-    } catch {
-        return data // fallback to original data if it can't be serialized.
-    }
-}
-
-let cityProvider = MoyaProvider<OpenWeathermap>(plugins: [NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter)])
+let cityProvider = MoyaProvider<OpenWeathermap>()
 
 // MARK: - Provider support
 
@@ -80,15 +68,4 @@ public func url(_ route: TargetType) -> String {
     return route.baseURL.appendingPathComponent(route.path).absoluteString
 }
 
-// MARK: - Response Handlers
-
-extension Moya.Response {
-    func mapNSArray() throws -> NSArray {
-        let any = try self.mapJSON()
-        guard let array = any as? NSArray else {
-            throw MoyaError.jsonMapping(self)
-        }
-        return array
-    }
-}
 
